dc4ee9ffd1bc063f7496fa6e93b855f7b3c589a7
Carla Lyra <dcarla421@gmail.com>
2021-03-08 13:24:50 -0500

    Model and Model Controller updated.

--- DreamTeam/DreamTeam/Controllers/Model Controller/DreamTeamController.swift
+++ DreamTeam/DreamTeam/Controllers/Model Controller/DreamTeamController.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import UIKit
+
+class DreamTeamController {
+    
+    static let share = DreamTeamController()
+    
+    var dreamTeamMembers: [DreamTeam] = []
+    
+    func createMember(name: String, photo: Image, bio: String, funFact: String) {
+        let newMember = DreamTeam(name: name, photo: photo, bio: bio, funFact: funFact)
+        
+        dreamTeamMembers.append(newMember)
+        saveToPersistenceStore()
+    }
+    
+    
+    func deleteMember(memberToDelete: DreamTeam) {
+        
+        guard let indexToDelete = dreamTeamMembers.firstIndex(of: memberToDelete) else { return }
+        
+        dreamTeamMembers.remove(at: indexToDelete)
+        saveToPersistenceStore()
+    }
+    
+    // MARK:  -Persistence
+    
+    // fileURL
+    func fileURL() -> URL {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let fileURL = urls[0].appendingPathComponent("DreamTeam.json")
+        return fileURL
+    }
+    //save
+    func saveToPersistenceStore() {
+        do {
+            let jsonDreamTeam = try JSONEncoder().encode(dreamTeamMembers)
+            try jsonDreamTeam.write(to: fileURL())
+        } catch {
+            print(error)
+            print(error.localizedDescription)
+        }
+    }
+    //load
+    func loadFromPersistenceStore() {
+        do {
+            let data = try Data(contentsOf: fileURL())
+            let decodedMembers = try JSONDecoder().decode([DreamTeam].self, from: data)
+            dreamTeamMembers = decodedMembers
+        } catch {
+            print(error)
+            print(error.localizedDescription)
+        }
+    }
+}//end of class
