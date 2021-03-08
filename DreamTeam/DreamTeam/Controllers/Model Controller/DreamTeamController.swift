a443ba55699e88c39998bfb3fde5f4031e478fa1
Carla Lyra <dcarla421@gmail.com>
2021-03-08 11:59:50 -0500

    Created group for each of the files.

---
+++ DreamTeam/DreamTeam/Controllers/Model Controller/DreamTeamController.swift
@@ -0,0 +1,8 @@
+//
+//  DreamTeamController.swift
+//  DreamTeam
+//
+//  Created by Carla Lyra on 3/8/21.
+//
+
+import Foundation
---
+++ DreamTeam/DreamTeam/Model/DreamTeam.swift
@@ -0,0 +1,8 @@
+//
+//  DreamTeam.swift
+//  DreamTeam
+//
+//  Created by Carla Lyra on 3/8/21.
+//
+
+import Foundation
